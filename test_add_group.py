b6d4b17d67110302abe86d05703797b59019c552
Tatiana Konischeva <moyzaldi@gmail.com>
2026-09-03 16:33:45 +0300

    записан тест, добавленны комментарии, выделены вспомогательные методы

--- test_add_group.py
+++ test_add_group.py
@@ -29,11 +29,18 @@ class TestAddGroup(unittest.TestCase):
         self.return_to_groups_page(wd)
         self.logout(wd)
 
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
 
-    def return_to_groups_page(self, wd):
-        wd.find_element_by_link_text("group page").click()
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_groups_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
 
     def create_group(self, wd, group):
         # init group creation
@@ -51,18 +58,11 @@ class TestAddGroup(unittest.TestCase):
         # submit  group creation
         wd.find_element_by_name("submit").click()
 
-    def open_groups_page(self, wd):
-        wd.find_element_by_link_text("groups").click()
+    def return_to_groups_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
 
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
 
     def tearDown(self):
         self.wd.quit()
