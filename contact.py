9d22eb591479a01e67b171a54eddff8292570202
Tatiana Konischeva <moyzaldi@gmail.com>
2026-09-03 16:50:03 +0300

    создан класс

---
+++ contact.py
@@ -0,0 +1,32 @@
+class Contact:
+    def __init__(self, firstname, middlename, lastname, title, company, nickname, address, home_phone,
+        mobile_phone, work_phone, email1, email2, email3, homepage, bday, bmonth, byear, aday, amonth,
+        ayear):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.title = title
+        self.company = company
+        self.nickname = nickname
+        self.address = address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.email1 = email1
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+
+
+
+
+
+
+
+
