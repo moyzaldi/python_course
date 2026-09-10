d850c80d561af13d704bf867dd91141efaa8f49d
Tatiana Konischeva <moyzaldi@gmail.com>
2026-09-10 14:38:38 +0300

    Замена устраревших  find_element_by на  find_element(By)

--- fixture/group.py
+++ fixture/group.py
@@ -1,53 +1,56 @@
+from selenium.webdriver.common.by import By
+
+
 class GroupHelper:
     def __init__(self, app):
         self.app = app
 
     def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        wd.find_element(By.LINK_TEXT, "groups").click()
 
     def create(self, group):
         wd = self.app.wd
         self.open_groups_page()
         # init group creation
-        wd.find_element_by_name("new").click()
+        wd.find_element(By.NAME, "new").click()
         # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element(By.NAME, "group_name").click()
+        wd.find_element(By.NAME, "group_name").clear()
+        wd.find_element(By.NAME, "group_name").send_keys(group.name)
+        wd.find_element(By.NAME, "group_header").click()
+        wd.find_element(By.NAME, "group_header").clear()
+        wd.find_element(By.NAME, "group_header").send_keys(group.header)
+        wd.find_element(By.NAME, "group_footer").click()
+        wd.find_element(By.NAME, "group_footer").clear()
+        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
         # submit  group creation
-        wd.find_element_by_name("submit").click()
+        wd.find_element(By.NAME, "submit").click()
         self.return_to_groups_page()
 
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
         # select first group
-        wd.find_element_by_name("selected[]").click()
+        wd.find_element(By.NAME, "selected[]").click()
         # submit deletion
-        wd.find_element_by_name("delete").click()
+        wd.find_element(By.NAME, "delete").click()
         self.return_to_groups_page()
 
     def edit_first_group(self,group):
         wd = self.app.wd
         self.open_groups_page()
         # select first group
-        wd.find_element_by_name("selected[]").click()
+        wd.find_element(By.NAME, "selected[]").click()
         # submit edition
-        wd.find_element_by_name("edit").click()
+        wd.find_element(By.NAME, "edit").click()
         # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("update").click()
+        wd.find_element(By.NAME, "group_name").click()
+        wd.find_element(By.NAME, "group_name").clear()
+        wd.find_element(By.NAME, "group_name").send_keys(group.name)
+        wd.find_element(By.NAME, "update").click()
         self.return_to_groups_page()
 
     def return_to_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("group page").click()
+        wd.find_element(By.LINK_TEXT, "group page").click()
