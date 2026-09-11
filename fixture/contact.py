fbf305ed33cbc146c73c0f911ec26688d204b095
Tatiana Konischeva <moyzaldi@gmail.com>
2026-09-11 12:34:34 +0300

    Исправлено название  текста ссылки

--- fixture/contact.py
+++ fixture/contact.py
@@ -101,7 +101,7 @@ class ContactHelper:
 
     def return_to_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        wd.find_element_by_link_text("home page").click()
 
 
 
